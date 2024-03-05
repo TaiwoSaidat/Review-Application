85eb648374587d0fa416d7174d17dfa415efb13d
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-03-05 12:31:58 +0100

    feat: add functionality to button and text length

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,31 +1,55 @@
 import { useState } from "react" 
 import Button from "./layout/Button"
 
-function ReviewForm() {
-    const [text, setText] = useState('')
+const spanStyle = {
+    display: 'block',
+    fontStyle: 'italic',
+    marginTop: '10px'
+}
 
+function ReviewForm() {
+//state for input text
+    const [text, setText] = useState('')
+//state for button disabled
+    const [btnDisabled, setBtnDisabled] = useState('true')
+//state for validation (characters)
+    const [msg, setMsg] = useState('')
+
+//function to update the text state in the input field
     const textHandler = (e) => {
+      if(text === ''){
+        setBtnDisabled(true)
+        setMsg(null)
+      } else if (text !== '' && text.trim().length <=20){
+        setMsg('your characters should be more than 20')
+        setBtnDisabled(true)
+      } else {
+        setMsg(null)
+        setBtnDisabled(false)
+      }
+      
         setText(e.target.value)
     }
   return (
     <>
       <div className="container">
       <div className="card">
-            <form action="">
+            <form >
                 <h3>Kindly drop a review of our services</h3>
                 <div className="input-group">
                     <input type="text" value={text} placeholder='Drop your review' onChange={textHandler} />
                     {/* <button type="submit">Submit</button> */}
-                    <Button type='submit' variant='secondary' >
+                    <Button type='submit' variant='secondary' isDisabled={btnDisabled} >
                         Submit
                     </Button>
-
                 </div>
+
+                {msg && (<span className="message" style={spanStyle}>{msg}</span>)}
             </form>
       </div>
       </div>
     </>
   )
-}
+  }
 
 export default ReviewForm
